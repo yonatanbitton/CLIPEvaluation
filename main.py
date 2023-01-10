f990fc57c46a3f66454444b6a8721efdfba0d7ee
Yonatan Bitton <yonatanbitton1@gmail.com>
2023-01-10 03:23:05 +0200

    first commit

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
+import json
 import os
 import numpy as np
-
 from PIL import Image
 from tqdm import tqdm
 from transformers import CLIPModel, CLIPProcessor
 
-annotations_file = '/Users/yonatanbitton/Documents/Flickr/flickr30k/filtered_test_by_flickr_entities.token'
-images_root = "/Users/yonatanbitton/Documents/Flickr/flickr30k-images"
+_FLICKR_ANNOTATIONS = '/Users/yonatanbitton/Documents/CLIPEvaluation/caption_datasets_KARPATY/dataset_flickr30k.json'
+_FLICKER_IMAGES = "/Users/yonatanbitton/Documents/CLIPEvaluation/Flickr/flickr30k-images"
+_FLICKR30 = 'flickr30'
 
+_MSCOCO_ANNOTATIONS = '/Users/yonatanbitton/Documents/CLIPEvaluation/caption_datasets_KARPATY/dataset_coco.json'
+_MSCOCO_IMAGES = "/Users/yonatanbitton/Documents/CLIPEvaluation/Flickr/flickr30k-images"
+_MSCOCO = 'mscoco'
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--clip_backend', default='clip-vit-base-patch32', choices=['clip-vit-base-patch32'],
+                    help='The name of the file to process')
+parser.add_argument('--dataset', default=_MSCOCO, choices=[_FLICKR30, _MSCOCO], help='The name of the file to process')
+args = parser.parse_args()
 
 def main():
+    print(f"Dataset: {args.dataset}")
+    # Get Image Retrieval Dataset
+    all_captions, all_images = get_ir_dataset()
+    print(f"Aggregated {len(all_images)} images and {len(all_captions)} captions")
+
     # Initialize CLIP model and tokenizer
     clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
     clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    dataset = open(annotations_file).readlines()
-    print(f"Read {len(dataset)} lines")
-    all_images = []
-    all_captions = []
-
-    for data in dataset:
-        # if len(all_images) > 100:
-        #     continue
-        imgname, caption = data.split("\t")
-        caption = caption.strip(".\n").strip()
-        imgname, imgnum = imgname.split("#")
-        if int(imgnum) > 0:
-            continue
-        all_images.append(imgname)
-        all_captions.append(caption)
-    print(f"Aggregated {len(all_images)} images and {len(all_captions)} captions")
 
     # Pre-compute image-text similarities
     similarities = np.empty((len(all_captions), len(all_images)))
@@ -36,14 +37,14 @@ def main():
         for j, imgname in enumerate(all_images):
             similarities[i, j] = get_img_txt_similarity(clip_model, clip_processor, imgname, txt)
 
-    # Find top 10 texts with highest similarity scores for each image
+    # Find top 10 texts with the highest similarity scores for each image
     top_texts_for_img = {}
     for j, imgname in enumerate(all_images):
         top_text_indices = similarities[:, j].argsort()[-10:][::-1]
         top_texts = [all_captions[i] for i in top_text_indices]
         top_texts_for_img[imgname] = top_texts
 
-    # Find top 10 images with highest similarity scores for each text
+    # Find top 10 images with the highest similarity scores for each text
     top_imgs_for_txt = {}
     for i, txt in enumerate(all_captions):
         top_img_indices = similarities[i, :].argsort()[-10:][::-1]
@@ -70,7 +71,21 @@ def main():
     print("Done")
 
 
+def get_ir_dataset():
+    path = _FLICKR_ANNOTATIONS if args.dataset == _FLICKR30 else _MSCOCO_ANNOTATIONS
+    dataset = json.load(open(path))
+    all_images = []
+    all_captions = []
+    for data in dataset['images']:
+        if data['split'] == 'test':
+            caption = data['sentences'][0]['raw']
+            all_images.append(data['filename'])
+            all_captions.append(caption)
+    return all_captions, all_images
+
+
 def get_img_txt_similarity(clip_model, clip_processor, imgname, txt):
+    images_root = _FLICKER_IMAGES if args.dataset == _FLICKR30 else _MSCOCO_IMAGES
     image = Image.open(os.path.join(images_root, imgname))
     inputs = clip_processor(text=[txt], images=image, return_tensors="pt", padding=True)
     outputs = clip_model(**inputs)
