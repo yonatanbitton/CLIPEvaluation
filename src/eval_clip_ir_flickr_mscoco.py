11a7a5be878099e4f090968951b6b42466aba42a
Yonatan Bitton <yonatanbitton1@gmail.com>
2023-01-13 04:24:00 +0200

    efficient

--- src/eval_clip_ir_flickr_mscoco.py
+++ src/eval_clip_ir_flickr_mscoco.py
@@ -22,10 +22,10 @@ _PREFIX = 'a photo of'
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--clip_backend', default='RN50', choices=['ViT-B/32', 'RN50'],
+parser.add_argument('--clip_backend', default='ViT-B/32', choices=['RN50', 'ViT-B/32', 'ViT-L/14', 'RN50x64'],
                     help='The CLIP backend version')
 parser.add_argument('--batch_size', type=int, default=200, help='Text batch size for each image. Batch size of 200 should fit with a single RTX2080.')
-parser.add_argument('--dataset', default=_FLICKR30, choices=[_FLICKR30, _MSCOCO],
+parser.add_argument('--dataset', default=_MSCOCO, choices=[_FLICKR30, _MSCOCO],
                     help='The name of the file to process')
 parser.add_argument('--add_prefix', action='store_const', default=True, help='Adds a prefix of "an image of a" to the textual prompt, following the original paper.', const=True)
 
@@ -58,36 +58,13 @@ def main():
             logits_per_image_batch, logits_per_text_batch = clip_model(image_tensor, tokenized_txt_batch.to(device))
             similarities[start:end, j] = logits_per_image_batch.detach().cpu().numpy()
 
-    # Find top 10 texts with the highest similarity scores for each image
-    top_texts_for_img = {}
-    for j, imgname in enumerate(all_images):
-        top_text_indices = similarities[:, j].argsort()[-10:][::-1]
-        top_texts = [all_captions[i] for i in top_text_indices]
-        top_texts_for_img[imgname] = top_texts
+    print(f'Text-Retrieval R@1: {text_recall_at_k(similarities, k=1)}')
+    print(f'Text-Retrieval R@5: {text_recall_at_k(similarities, k=5)}')
+    print(f'Text-Retrieval R@10: {text_recall_at_k(similarities, k=10)}')
 
-    # Find top 10 images with the highest similarity scores for each text
-    top_imgs_for_txt = {}
-    for i, txt in enumerate(all_captions):
-        top_img_indices = similarities[i, :].argsort()[-10:][::-1]
-        top_imgs = [all_images[j] for j in top_img_indices]
-        top_imgs_for_txt[txt] = top_imgs
-
-    # Compute mean R@1, R@5, and R@10 for text
-    mean_r_at_1_txt = compute_mean_r_at_k([top_imgs_for_txt[txt] for txt in all_captions], all_images, 1)
-    mean_r_at_5_txt = compute_mean_r_at_k([top_imgs_for_txt[txt] for txt in all_captions], all_images, 5)
-    mean_r_at_10_txt = compute_mean_r_at_k([top_imgs_for_txt[txt] for txt in all_captions], all_images, 10)
-
-    # Compute mean R@1, R@5, and R@10 for image
-    mean_r_at_1_img = compute_mean_r_at_k([top_texts_for_img[img] for img in all_images], all_captions, 1)
-    mean_r_at_5_img = compute_mean_r_at_k([top_texts_for_img[img] for img in all_images], all_captions, 5)
-    mean_r_at_10_img = compute_mean_r_at_k([top_texts_for_img[img] for img in all_images], all_captions, 10)
-    print(
-        f"Mean R@1 for text: {mean_r_at_1_txt:.1f}  "
-        f"Mean R@5 for text: {mean_r_at_5_txt:.1f}  "
-        f"Mean R@10 for text: {mean_r_at_10_txt:.1f}  "
-        f"Mean R@1 for image: {mean_r_at_1_img:.1f}  "
-        f"Mean R@5 for image: {mean_r_at_5_img:.1f}  "
-        f"Mean R@10 for image: {mean_r_at_10_img:.1f}")
+    print(f'Image-Retrieval R@1: {image_recall_at_k(similarities, k=1)}')
+    print(f'Image-Retrieval R@5: {image_recall_at_k(similarities, k=5)}')
+    print(f'Image-Retrieval R@10: {image_recall_at_k(similarities, k=10)}')
 
     print("Done")
 
@@ -98,25 +75,46 @@ def prepare_ir_dataset():
     all_images = []
     all_captions = []
     for data in dataset['images']:
-        if len(all_images) > 50:
+        if len(all_images)>20:
             break
         if data['split'] == 'test':
-            caption = data['sentences'][0]['raw']
-            if args.add_prefix:
-                caption = f"{_PREFIX} {caption}"
             all_images.append(data['filename'])
-            all_captions.append(caption)
+            for caption in data['sentences'][:5]:
+                if args.add_prefix:
+                    caption = f"{_PREFIX} {caption['raw']}"
+                all_captions.append(caption)
     return all_captions, all_images
 
+def text_recall_at_k(similarities, k):
+    ''' the images are used to retrieve the corresponding sentences '''
+    n_images = similarities.shape[1]
+    recall = 0
+    for i in range(n_images):
+        # Find the top K captions for each image
+        top_k_captions = similarities[:, i].argsort()[-k:][::-1][:k]
+        # Check if at least one of the relevant captions is in the top K
+        relevant_captions = np.arange(i*5, (i+1)*5)
+        if any(np.isin(relevant_captions, top_k_captions)):
+            recall += 1
+    # Calculate the overall recall
+    recall = recall / n_images
+    return recall
 
-def compute_mean_r_at_k(rankings, labels, k):
-    r_at_k = []
-    for ranking, label in zip(rankings, labels):
-        if label in ranking[:k]:
-            r_at_k.append(1)
-        else:
-            r_at_k.append(0)
-    return np.mean(r_at_k) * 100
+def image_recall_at_k(similarities, k):
+    ''' the sentences are used to retrieve the corresponding images '''
+    n_captions = similarities.shape[0]
+    recall = 0
+    for i in range(n_captions):
+        # Find the top K images for each caption
+        top_k_images = similarities[i, :].argsort()[::-1][:k]
+
+        # Check if at least one of the relevant images is in the top K
+        relevant_images = [int(i/5)]
+        if any(np.isin(relevant_images, top_k_images)):
+            recall += 1
+    # Calculate the overall recall
+    recall = recall / n_captions
+    return recall
 
 
 if __name__ == '__main__':
